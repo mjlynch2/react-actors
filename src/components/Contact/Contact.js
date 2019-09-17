75f3cc11734867896bc5dec587ef60bc71188279
Mike Lynch <mjlynch2@gmail.com>
2019-09-17 16:46:42 -0500

    make the stretch goal work

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'; // Object destructuring
 
 class Contact extends Component { // classes mass produce objects
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            LastName: '',
+            bestImpression: '',
+            sentence: ''
+        }
+    }
+
+    handleFirst = ((e) => {
+        this.setState({ firstName: e.target.value })
+    })
+    
+    handleLast = ((e) => {
+        this.setState({ lastName: e.target.value })
+    })
+    
+    handleChange = ((e) => {
+        this.setState({ bestImpression: e.target.value,
+            sentence: <span>{this.state.firstName} {this.state.lastName}'s best impression is {this.state.bestImpression}</span>})
+    })
+
     render() {
         return (
             <div>
                 <h2>Contact</h2>
                 <p>PHONE: 1800-WEREACT</p>
+                <input id="firstName" onChange={this.handleFirst}></input><input id="lastName" onChange={this.handleLast}></input><input id="bestImpression" onChange={this.handleChange}></input>
+                <p>{this.state.sentence}</p>
             </div>
         );
     }
